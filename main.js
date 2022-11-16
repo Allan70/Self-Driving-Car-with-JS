46ff35695f3b01623caf6b0a002d6dcd49a0f991
Abere Allan <aabere70@gmail.com>
2022-11-16 12:21:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const N = 500;
+const N = 600;
 const cars = generateCars(N)
 let bestCar = cars[0];
 
